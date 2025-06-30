5f645a5e93b89acedd79b06e3387260483c7e53a
michaelTheKing0616 <47001117+michaelTheKing0616@users.noreply.github.com>
2025-06-30 19:34:59 +0100

    Update __init__.py
    pass results of each function to the next call in case of no value returned in that function

--- orchestrator_function/__init__.py
+++ orchestrator_function/__init__.py
@@ -2,9 +2,9 @@
 import azure.durable_functions as df
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
-    yield context.call_activity("FetchFirebaseData", None)
-    yield context.call_activity("TriggerPrediction", None)
-    yield context.call_activity("OptimizeEnergy", None)
-    return "Workflow complete"
+    result1 = yield context.call_activity("FetchFirebaseData", None)
+    result2 = yield context.call_activity("TriggerPrediction", result1)
+    result3 = yield context.call_activity("OptimizeEnergy", result2)
+    return [result1, result2, result3]
 
 main = df.Orchestrator.create(orchestrator_function)
