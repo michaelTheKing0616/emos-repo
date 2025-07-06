d6120ae181cbd542492c6b2a66c323a86b9c8961
michaelTheKing0616 <47001117+michaelTheKing0616@users.noreply.github.com>
2025-07-06 13:23:56 +0100

    Update __init__.py

--- starter_function/__init__.py
+++ starter_function/__init__.py
@@ -1,8 +1,17 @@
+# starter_function/__init__.py
 import azure.functions as func
 import azure.durable_functions as df
 import logging
+from datetime import datetime
 
 async def main(mytimer: func.TimerRequest, starter: str):
-    client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new("orchestrator_function", None)
-    logging.info(f"Started orchestration with ID = '{instance_id}'")
+    utc_timestamp = datetime.utcnow().isoformat()
+    logging.info(f"starter_function triggered at {utc_timestamp}")
+
+    try:
+        client = df.DurableOrchestrationClient(starter)
+        instance_id = await client.start_new("orchestrator_function", None)
+
+        logging.info(f"Started orchestration with ID = '{instance_id}'")
+    except Exception as e:
+        logging.error(f"Failed to start orchestration: {str(e)}", exc_info=True)
